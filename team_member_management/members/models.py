168fa93268ef6f3df7e6416ff59bdf44fc16020a
Prachi Shah <prachi312@gmail.com>
2024-08-08 07:45:07 -0700

    PR comments addressed. Added data validation, improved delete page styling, updated members list page

--- team_member_management/members/models.py
+++ team_member_management/members/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
 # Create TeamMember model.
 
@@ -7,11 +8,16 @@ class TeamMember(models.Model):
         ('regular', 'Regular'),
         ('admin', 'Admin'),
     ]
+
+    phone_number_regex = RegexValidator(
+        regex=r'^\+?\d+(-\d+)*$',
+        message="Enter the correct phone number."
+    )
     
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    phone_number = models.CharField(max_length=15)
-    email = models.EmailField()
+    first_name = models.CharField("First Name", max_length=100, blank=False, null=False)
+    last_name = models.CharField("Last Name", max_length=100, blank=False, null=False)
+    phone_number = models.CharField("Phone Number", max_length=15, blank=False, null=False, validators=[phone_number_regex])
+    email = models.EmailField("Email", max_length=200, unique=True, blank=False, null=False)
     role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='regular')
 
     def __str__(self):
